21db53a076be7d47c5dd99a9279fe917695229bb
Alex Sharp <ajsharp@gmail.com>
2008-07-27 02:24:16 -0500

    Fixed controller to generator correctly with Admin::Prefix

--- templates/controller.rb
+++ templates/controller.rb
@@ -1,77 +1,53 @@
 class <%= controller_class_name %>Controller < ApplicationController
-  before_filter :check_access, :except => [:index, :show]
-  
-  # GET /<%= table_name %>
+  # GET /<%= file_name %>
   def index
-    @<%= table_name %> = <%= class_name %>.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-    end
+    @<%= controller_singular_name.downcase %> = <%= controller_singular_name %>.find(:all)
   end
 
-  # GET /<%= table_name %>/1
+  # GET /<%= file_name %>/1
   def show
-    @<%= file_name %> = <%= class_name %>.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-    end
+    @<%= controller_singular_name.downcase %> = <%= controller_singular_name %>.find(params[:id])
   end
 
-  # GET /<%= table_name %>/new
+  # GET /<%= file_name %>/new
   def new
-    @<%= file_name %> = <%= class_name %>.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-    end
+    @<%= controller_singular_name.downcase %> = <%= controller_singular_name %>.new
   end
 
-  # GET /<%= table_name %>/1/edit
+  # GET /<%= file_name %>/1/edit
   def edit
-    @<%= file_name %> = <%= class_name %>.find(params[:id])
-    
-    respond_to do |format|
-      format.html # edit.html.erb
-    end
+    @<%= controller_singular_name.downcase %> = <%= controller_singular_name %>.find(params[:id])
   end
 
-  # POST /<%= table_name %>
+  # POST /<%= file_name %>
   def create
-    @<%= file_name %> = <%= class_name %>.new(params[:<%= file_name %>])
+    @<%= controller_singular_name.downcase %> = <%= controller_singular_name %>.new(params[:<%= controller_singular_name.downcase %>])
 
-    respond_to do |format|
-      if @<%= file_name %>.save
-        flash[:notice] = '<%= class_name %> was successfully created.'
-        format.html { redirect_to(@<%= file_name %>) }
-      else
-        format.html { render :action => "new" }
-      end
+    if @<%= controller_singular_name.downcase %>.save
+      flash[:notice] = '<%= controller_singular_name %> was successfully created.'
+      redirect_to([:admin, @<%= controller_singular_name.downcase %>])
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /<%= table_name %>/1
+  # PUT /<%= file_name %>/1
   def update
-    @<%= file_name %> = <%= class_name %>.find(params[:id])
+    @<%= controller_singular_name.downcase %> = <%= controller_singular_name %>.find(params[:id])
 
-    respond_to do |format|
-      if @<%= file_name %>.update_attributes(params[:<%= file_name %>])
-        flash[:notice] = '<%= class_name %> was successfully updated.'
-        format.html { redirect_to(@<%= file_name %>) }
-      else
-        format.html { render :action => "edit" }
-      end
+    if @<%= controller_singular_name.downcase %>.update_attributes(params[:<%= controller_singular_name.downcase %>])
+      flash[:notice] = '<%= controller_singular_name %> was successfully updated.'
+      redirect_to([:admin, @<%= controller_singular_name.downcase %>])
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /<%= table_name %>/1
+  # DELETE /<%= file_name %>/1
   def destroy
-    @<%= file_name %> = <%= class_name %>.find(params[:id])
+    @<%= file_name %> = <%= controller_singular_name %>.find(params[:id])
     @<%= file_name %>.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(<%= table_name %>_url) }
-    end
+    redirect_to(<%= table_name %>_url)
   end
 end
